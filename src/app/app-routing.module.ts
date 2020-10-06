92a8e8a314632d1fbf8cfeb9284d28f01a3b48bb
Mikail Esim <mikail__esim66@hotmail.com>
2020-10-06 17:35:35 +0300

    Data Binding, Cookie ve Local Storage Oluşturuldu. #7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,14 @@ const routes: Routes = [
     loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   },
   {
+    path: "data-binding",
+    loadChildren: () => import('./data-binding/data-binding.module').then(m => m.DataBindingModule)
+  },
+  {
+    path: "storage",
+    loadChildren: () => import('./storage/storage.module').then(m => m.StorageModule)
+  },
+  {
     path: "**",
     component: HomeComponent
   },
